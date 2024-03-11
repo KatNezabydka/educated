757f339851178e5dd9f9d58ec02fda9d9b8cfa8c
Maksym_enKO <finside41@gmail.com>
2024-03-12 00:44:27 +0200

    colorama was added with show_help func

--- src/Birthday.py
+++ src/Birthday.py
@@ -1,20 +1,21 @@
 from Field import Field
-from  ValidationError import ValidationError
+from ValidationError import ValidationError
 from datetime import datetime
+
 
 def validate_birthday(func):
     def wrapper(*args, **kwargs):
         value = args[1]
         try:
-           datetime.strptime(value, '%d.%m.%Y')
+            datetime.strptime(value, "%d.%m.%Y")
         except Exception:
             raise ValidationError("Incorrect date format. Use DD.MM.YYYY.")
         return func(*args, **kwargs)
+
     return wrapper
+
 
 class Birthday(Field):
     @validate_birthday
     def __init__(self, value):
         super().__init__(datetime.strptime(value, "%d.%m.%Y"))
-    
-
--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,18 @@
-
 from AddressBook import AddressBook
 from Record import Record
 from ValidationError import ValidationError
 
+# lib imports
+from colorama import init, Fore, Style, Back
+
 ERROR_MESSAGES = {
-    ValueError: "Give me name and phone/birthday please.",
+    ValueError: f"{Fore.YELLOW} Give me name and phone/birthday please.",
     ValidationError: lambda e: str(e),
-    KeyError: "Contact not found.",
-    AttributeError: "Contact not found.",
-    IndexError: "Give me name."
+    KeyError: f"{Fore.RED} Contact not found.",
+    AttributeError: f"{Fore.RED} Contact not found.",
+    IndexError: f"{Fore.YELLOW} Give me name.",
 }
+
 
 def input_error(func):
     def inner(*args, **kwargs):
@@ -24,7 +27,41 @@ def input_error(func):
                     return error_message
             else:
                 raise e
+
     return inner
+
+
+def show_help():
+    # Print available commands with color highlighting
+    print("*" * 30 + " < HELP MENU START > " + "*" * 30)
+    print(Fore.MAGENTA + Style.BRIGHT + "Available commands:")
+    print(Fore.GREEN + "--> hello: Get a greeting from the bot.")
+    print(
+        Fore.YELLOW
+        + "--> add [name] [phone]: Add a new contact with the name and phone number."
+    )
+    print(
+        Fore.YELLOW
+        + "--> change [name] [new phone]: Change the phone number for the specified contact."
+    )
+    print(
+        Fore.YELLOW
+        + "--> phone [name]: Show the phone number for the specified contact."
+    )
+    print(Fore.YELLOW + "--> all: Show all contacts in the address book.")
+    print(
+        Fore.YELLOW
+        + "--> add-birthday [name] [birthday]: Add a birthday for the specified contact."
+    )
+    print(
+        Fore.YELLOW
+        + "--> show-birthday [name]: Show the birthday for the specified contact."
+    )
+    print(Fore.YELLOW + "--> birthdays: Show birthdays happening within the next week.")
+    print(Fore.RED + "--> close or exit: Close the program.")
+    print("*" * 30 + " < HELP MENU END > " + "*" * 30)
+    print(Style.RESET_ALL)  # Reset colors
+
 
 @input_error
 def parse_input(user_input):
@@ -32,51 +69,57 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+
 @input_error
 def add_contact(args: list, book: AddressBook) -> str:
     name, phone = args
-    if book.find(name) == None:
+    if book.find(name) is None:
         book.add_record(Record(name).add_phone(phone))
-        return "Contact added."
-    return "Contact already exist."
+        return f"{Fore.GREEN} Contact added."
+    return f"{Fore.YELLOW} Contact already exist."
+
 
 @input_error
 def change_contact(args: list, book: AddressBook) -> str:
     name, phone = args
     contact = book.find(name)
-    if  contact != None:
+    if contact is not None:
         contact.delete_phones().add_phone(phone)
-        return "Contact updated."
-    return "Contact not found."
-    
+        return f"{Fore.GREEN} Contact updated."
+    return f"{Fore.RED} Contact not found."
+
 
 @input_error
 def show_phone(args: list, book: AddressBook) -> str:
     name = args[0]
     return book.find(name).print_phones()
-    
+
+
 def show_all(book: AddressBook) -> print:
     if len(book.data) == 0:
-        print("The list is empty")
+        print(Fore.RED + "The list is empty" + " ¯\\_(ツ)_/¯")
     for name, record in book.data.items():
         print(f"{name}. {record.print_phones()}")
-  
+
+
 @input_error
 def add_birthday(args: list, book: AddressBook) -> str:
     name, birthday = args
     record = book.find(name)
-    if record != None and record.has_birthday() == False:
+    if record is not None and record.has_birthday() is False:
         record.add_birthday(birthday)
-        return "Birthday added."
-    return "Contact not found or birthday already exist."  
+        return f"{Fore.GREEN} Birthday added."
+    return f"{Fore.YELLOW} Contact not found or birthday already exist."
+
 
 @input_error
 def show_birthday(args: list, book: AddressBook) -> str:
     name = args[0]
     record = book.find(name)
-    if record != None and record.has_birthday() != False:
+    if record is not None and record.has_birthday() is not False:
         return record.show_birthday()
-    return "Contact not found or birthday is empty"  
+    return f"{Fore.YELLOW} Contact not found or birthday is empty"
+
 
 def birthdays(book: AddressBook) -> print:
     if len(book.data) == 0:
@@ -85,17 +128,32 @@ def birthdays(book: AddressBook) -> print:
         if names:
             print(f"{day}: {', '.join(names)}")
 
+
 def main():
+    # Initialize colorama for color support in the console
+    init(autoreset=True)
+
     book = AddressBook()
     commands_to_close = ["close", "exit"]
-    print("Welcome to the assistant bot!")
+    print(
+        Back.WHITE
+        + Fore.BLACK
+        + Style.BRIGHT
+        + "🌟 Welcome to the assistant bot! 🌟 Type "
+        + Back.GREEN
+        + "help"
+        + Back.WHITE
+        + " for assistance."
+    )
     while True:
-        user_input = input("Enter a command: ")
+        user_input = input(Fore.MAGENTA + "Enter a command: ")
         command, *args = parse_input(user_input)
-        
+
         match command:
+            case "help":
+                show_help()
             case "hello":
-                 print("How can I help you?")
+                print(Fore.CYAN + "How can I help you? 😊")
             case "add":
                 print(add_contact(args, book))
             case "change":
@@ -111,10 +169,13 @@ def main():
             case "birthdays":
                 birthdays(book)
             case _ if command in commands_to_close:
-                print("Good bye!")
+                print(Fore.GREEN + "Good bye! 😊")
                 break
             case _:
-                 print("Invalid command.")
+                print(
+                    Fore.RED + "⚠️ Invalid command. Please type 'help' for assistance. ⚠️"
+                )
+
 
 if __name__ == "__main__":
     main()
