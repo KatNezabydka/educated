8d9d5eaa3098f6248099e7e3c66c6ebb457ded1c
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2024-03-13 13:48:48 +0100

    Update Name.py

--- src/Name.py
+++ src/Name.py
@@ -6,10 +6,8 @@ from ValidationError import ValidationError
 def validate_name(func):
     def wrapper(value):
         if not value.isalpha():
-            print("The name must consist of letters only")
             raise ValidationError("The name must consist of letters only")
         if not value.istitle():
-            print("The name must start with an upper case letter and the rest letters must be lower case")
             raise ValidationError("The name must start with an upper case letter and the rest letters must be lower case")
         return func
     return wrapper
