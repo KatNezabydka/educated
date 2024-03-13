74c3ce4785a81b6dbb08411e79bc02068ac39398
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2024-03-13 11:18:54 +0100

    apdate class Name

--- src/Name.py
+++ src/Name.py
@@ -2,4 +2,11 @@ from Field import Field
 
 
 class Name(Field):
-    pass
+    def __init__(self, value: str):
+        if not value.isalpha():
+            print("The name must consist letters only")
+            raise ValueError
+        if not value.istitle():
+            print("The name must start with an upper case letter and the rest letter must be lower case")
+            raise ValueError
+        self.value = value
